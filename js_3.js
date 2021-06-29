133fe95688fcb07a35628f29c136e2c03e86c896
jae0eaj <86229084+jae0eaj@users.noreply.github.com>
2021-06-29 13:14:13 +0900

    Delete js_3.js

--- js_3.js
+++
@@ -1,8 +0,0 @@
-
-let multiMenu = document.querySelector('#multiMenu');
-let subMenu = document.querySelector('#subMenu');
-
-multiMenu.addEventListener('mouseover', ()=> 
-{subMenu.classList.toggle('show');
-});
-
